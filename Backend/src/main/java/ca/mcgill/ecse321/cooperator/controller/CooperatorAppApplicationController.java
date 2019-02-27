33caa7cfe09eb742477fae5a8a829609837bc33b
Jason Ra <rahaljas@gmail.com>
2019-02-27 14:22:57 -0500

    :bug:

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorAppApplicationController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorAppApplicationController.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -170,4 +171,11 @@ public class CooperatorAppApplicationController {
 	}
 	
 	
+// EventDto --> Event
+@GetMapping(value = { "/events/{name}", "/events/{name}/" })
+public EventDto getEventByName(@PathVariable("name")
+String name) throws IllegalArgumentException {
+return convertToDto(service.getEvents(name));
+}
+
 }
