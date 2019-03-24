f1f4d485ad533fd8aa2f492a7dfcf5a744269883
edgar c <tsung-hsun.chang@mail.mcgill.ca>
2019-03-24 00:32:17 -0400

    updated loginpage html

--- Frontend/src/components/LoginPage.vue
+++ Frontend/src/components/LoginPage.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="loginpage">
+    <h1 align = "center">
+      Login
+    </h1>
+
+    <table align="center">
+
+      <td align = "center">
+        <h4>
+          Enter Employer ID
+        </h4>
+        <input type = "text" size= "25"></input>
+      </td>
+
+      <td>
+
+      </td>
+
+
+    </table>
   </div>
+
 </template>
 
 <script>
