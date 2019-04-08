eea8ff23f55567edba7502d8086401c644bb199b
Alphagithub11 <mashood.ahmed@mail.mcgill.ca>
2019-04-07 21:03:55 -0400

    Fronend Changes

---
+++ Frontend/src/components/ConfirmConfirmation.vue
@@ -0,0 +1,170 @@
+<template>
+  <div id="dashboard">
+    <table id="head" align="left" style="width: 100%">
+      <tr>
+        <td>
+          <h1 align="left" id="dashboard1">Welcome Employer</h1>
+        </td>
+        <td>
+          <h1 align="right" id="dashboard1">Employer ID: {{employerID}}</h1>
+        </td>
+      </tr>
+    </table>
+    <ul>
+      <li>
+        <h5>
+          <a>
+            <router-link :to="{ name: 'Dashboard', params: {employerID: employerID} }">Dashboard</router-link>
+          </a>
+        </h5>
+      </li>
+      <li>
+        <h5>
+          <a>
+            <router-link
+              :to="{ name: 'ConfirmStudentStart', params: {employerID: employerID} }"
+            >Confirm Student Start</router-link>
+          </a>
+        </h5>
+      </li>
+      <li>
+        <h5>
+          <a class="active">
+            <router-link
+              :to="{ name: 'ConfirmStudentCompletion', params: {employerID: employerID}}"
+            >Confirm Student Completion</router-link>
+          </a>
+        </h5>
+      </li>
+      <li>
+        <h5>
+          <a>
+            <router-link
+              :to="{ name: 'DownloadCoopForm', params: {employerID: employerID} }"
+            >Download Co-op Placement Form</router-link>
+          </a>
+        </h5>
+      </li>
+      <li>
+        <h5>
+          <a>
+            <router-link
+              :to="{ name: 'DownloadTaxCreditForm', params: {employerID: employerID} }"
+            >Download Tax Credit Form</router-link>
+          </a>
+        </h5>
+      </li>
+    </ul>
+
+    <div id="createAccount">
+      <h2>Register Student</h2>
+      <form id="createForm" @submit="confirm()" action="javascript:void(0);">
+        <p>
+          <label for="coopPositionID">Coop Position</label>
+          <select v-model="irmakselected" required>
+            <option
+              v-bind:key="coop.id"
+              v-for="coop in irmaklist"
+              v-bind:value="coop"
+            >{{coop.id}}-{{coop.companyName}}-{{coop.positionName}}</option>
+          </select>
+        </p>
+
+        <p>
+          <label for="studentID">Student ID</label>
+          <input
+            type="number"
+            name="Student ID"
+            v-bind:value="irmakselected.student"
+            readonly
+            id="Student ID"
+            placeholder="Student ID"
+          >
+        </p>
+
+        <p>
+          <label for="password">End Date</label>
+          <input
+            type="date"
+            name="password"
+            v-model="evaluationDate"
+            id="password"
+            placeholder="Start Date"
+            required
+          >
+        </p>
+
+        <p>
+          <label for="url">Google Drive Link for Evaluation Form</label>
+          <input type="text" name="URL" id="Url" placeholder="URL">
+        </p>
+
+        <p>
+          <input type="submit" value="Confirm Start">
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<script src="./ConfirmStudentCompletion2.js">
+</script>
+
+<style>
+#head {
+  background-color: #696969;
+}
+#dashboard1 {
+  color: white;
+}
+#dashborard {
+  padding-left: 20px;
+}
+h2 {
+  color: white;
+}
+h2 {
+  padding-right: 20px;
+}
+#progress {
+  margin-top: 150px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  width: 25%;
+  background-color: grey;
+  position: absolute;
+  height: 100%;
+  overflow: auto;
+  border-color: white;
+  margin-top: 58px;
+}
+li a {
+  display: block;
+  color: white;
+  padding: 20px 16px;
+  text-decoration: none;
+  border-color: white;
+}
+li a.active {
+  background-color: #4caf50;
+  color: white;
+}
+li a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+  border-color: white;
+}
+#box1 {
+  margin-top: 200%;
+  color: grey;
+  border: black;
+}
+#body {
+  margin-top: 30px;
+  width: 125%;
+}
+</style>
+
---
+++ Frontend/src/components/ConfirmConfirmation2.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import Vue from 'vue'
+import Router from 'vue-router'
+import Axios from 'axios';
+
+Vue.use(Router)
+
+
+var config = require('../../config')
+
+
+var AXIOS = axios.create({
+    baseURL: config.backendUrl,
+    headers: { 'Access-Control-Allow-Origin': config.frontendUrl }
+})
+
+export default {
+    name: 'ConfirmStudentStart',
+    data() {
+        return {
+            startConfirmation: [],
+            newStartConfirmation: '',
+            errorStartConfirmation: '',
+            employerID: null,
+            evaluationDate: null,
+            coopPositionID: null,
+            studentID: null,
+            response: [],
+            errors: [],
+            opps: false,
+            irmaklist: [],
+            irmakselected: ''
+        }
+    },
+    created: function () {
+        this.employerID = this.$route.params.employerID;
+        this.updateIrmakList();
+
+    },
+    methods: {
+        createStartConfirmation: function () {
+            this.errors = []
+            var random = Math.trunc(Math.random() * 100000000)
+            //mydate = this.evaluationDate.getFullYear() + '-' + (this.evaluationDate.getMonth() + 1) + '-' + this.evaluationDate.getDate()
+            AXIOS.post('/StartConfirmation/' + random + "?evaluationDate=" + this.evaluationDate + "&coopPositionID=" + this.irmakselected.id + "&studentID=" + this.irmakselected.student + "&employerID=" + this.employerID, {}, {})
+
+                .then(response => {
+                    this.startConfirmation.push(response.data)
+                    this.newStartConfirmation = ''
+                    this.errorStartConfirmation = ''
+                    console.log(response.data)
+                })
+                .catch(e => {
+                    var errorMsg = e.message;
+                    this.errors.push(e.message);
+                    console.log(errorMsg);
+                    this.errors.push("Username is required");
+                });
+        },
+        updateIrmakList: function () {
+            console.log('elias')
+            AXIOS.get('/irmak/' + this.employerID, {}, {})
+                .then(response => {
+                    this.irmaklist = response.data
+                    console.log(this.irmaklist)
+                })
+                .catch(e => {
+                    var errorMsg = e.message;
+                    this.errors.push(e.message);
+                    console.log(errorMsg + 'here');
+                    this.errors.push("irmak endpoint did not work");
+                })
+        },
+        confirm: function () {
+            window.alert("Success! Student's Coop Position is confirmed ")
+        }
+
+    }
+}
+
+
